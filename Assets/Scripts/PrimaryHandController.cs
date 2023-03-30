3100e53edd48128132342c79c0154113bb02eb90
zachycardia <zachycardian@gmail.com>
2023-03-31 05:58:26 +0800

    Reorganize controller scripts & controller controls
    - Implement base HandController script.
    - Add fields for ray & direct interactors in secondary hand controller.
    - Disable teleport action.
    - Implement continuous move.

--- Assets/Scripts/PrimaryHandController.cs
+++ Assets/Scripts/PrimaryHandController.cs
@@ -3,17 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class PrimaryHandController : MonoBehaviour
+public class PrimaryHandController : HandControllerBase
 {
-    [SerializeField] private HandVisualizer visualizer;
 
-    public void OnSelectEnter(SelectEnterEventArgs args)
-    {
-        visualizer.SetState(HandVisualizer.State.Select);
-    }
-
-    public void OnSelectExit(SelectExitEventArgs args)
-    {
-        visualizer.SetState(HandVisualizer.State.Neutral);
-    }
 }
