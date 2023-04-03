76f1a8f5e801a91c9d05b3341abe9e2e8d238fb5
zachycardia <zachycardian@gmail.com>
2023-04-03 14:03:10 +0800

    Integrate sauce gun code

--- Assets/Scripts/Tool/SauceGun.cs
+++ Assets/Scripts/Tool/SauceGun.cs
@@ -1,15 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class SauceGun : MonoBehaviour
+public class SauceGun : InteractableBase
 {
-    [SerializeField] GameObject BallSpawnPoint;
-    [SerializeField] GameObject Ball;
+    [SerializeField] private Vector3 spawnOffset;
+    [SerializeField] private GameObject projectileObject;
+    [SerializeField] private float fireInterval;
+    [SerializeField] private float projectileSpeed;
 
-    private void Update() {
-        //if(Input.GetButtonDown("Fire1")) {
-            Instantiate(Ball, BallSpawnPoint.transform.position, BallSpawnPoint.transform.rotation);
-        //}
+    private bool isFiring = false;
+    private float fireTimer;
+
+    public override void OnActivate(ActivateEventArgs args)
+    {
+        base.OnActivate(args);
+        isFiring = true;
+    }
+
+    public override void OnDeactivate(DeactivateEventArgs args)
+    {
+        base.OnDeactivate(args);
+        isFiring = false;
+    }
+
+    private void Update()
+    {
+        if (isFiring)
+        {
+            fireTimer -= Time.deltaTime;
+            if (fireTimer <= 0)
+            {
+                ShootOnce();
+                fireTimer = fireInterval;
+            }
+        }
+    }
+
+    private void ShootOnce()
+    {
+        Vector3 spawnPosition = transform.TransformPoint(spawnOffset);
+        Instantiate(projectileObject, spawnPosition, transform.rotation);
+        projectileObject.GetComponent<Rigidbody>().velocity = projectileSpeed * projectileObject.transform.forward;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.TransformPoint(spawnOffset), 0.02f);
     }
 }
