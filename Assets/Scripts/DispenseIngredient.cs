019c8f423a88bd93028e72dd6092187e04ec5b32
Kyle Vincent <KyleVsquash@gmail.com>
2023-04-03 12:21:00 +0800

    Pushing Update
    - Adding Interactability to Ingredient Dispenser

---
+++ Assets/Scripts/DispenseIngredient.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispenseIngredient : MonoBehaviour
+{
+    [SerializeField] GameObject Ingredient;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(Ingredient, this.transform.position, this.transform.rotation);
+    }
+}
